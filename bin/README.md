007ac2dd07e43cbdb8350f841a15e49090791319
Ezra Thomas <Ezra.thomas@baxter-academy.org>
2015-10-23 12:46:17 -0400

    modification to pneumatics code

---
+++ bin/README.md
@@ -0,0 +1,5 @@
+Outlier1
+=========
+[![Build Status](https://travis-ci.org/frc5687/Outlier1.svg)](https://travis-ci.org/frc5687/Outlier1)
+
+Code for FRC5687's 2015 robot
--- src/org/usfirst/frc/team5687/robot/subsystems/pneumatics.java
+++ src/org/usfirst/frc/team5687/robot/subsystems/pneumatics.java
@@ -1,43 +1,33 @@
 package org.usfirst.frc.team5687.robot.subsystems;
-
+import org.usfirst.frc.team5687.robot.OI;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import org.usfirst.frc.team5687.robot.OI.pistonOff;
-public class pneumatics {
 
-	DoubleSolenoid  testDouble = new DoubleSolenoid(1, 2); 
+public class pneumatics{
+	OI testOi;
+	boolean pistonOff;
+	DoubleSolenoid testDouble; 
 	
 	public void Test(){
-	if(PistonOff){
-		
-		testDouble.set(DoubleSolenoid.Value.kOff);
+		testOi = new OI();
+		pistonOff = testOi.pistonOff();
+		testDouble = new DoubleSolenoid(1, 2); 
+		if(pistonOff){
+			
+			testDouble.set(DoubleSolenoid.Value.kOff);
+		}
 	}
-	}
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	public void pistonOut() throws InterruptedException { 
-	if(getPistonOffButton){
-		testDouble.set(DoubleSolenoid.Value.kOff);
-	}
-	}
-		
-		
-		
-	}
-
 }
 
-testDouble.set(DoubleSolenoid.Value.kForward);
-Thread.sleep(1000);
-		testDouble.set(DoubleSolenoid.Value.kReverse);
-		Thread.sleep(1000);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 		
